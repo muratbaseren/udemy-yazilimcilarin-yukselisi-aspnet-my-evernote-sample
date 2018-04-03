da778e4601a2846275b6b3ccf3f6d470636c914a
Murat Başeren <muratbaseren@gmail.com>
2018-04-03 03:03:05 +0300

    Kategori silme işleminde karşılaşılan ilişkili veri sebebiyle silinememesinin çözülmesi.

--- MyEvernote.BusinessLayer/CategoryManager.cs
+++ MyEvernote.BusinessLayer/CategoryManager.cs
@@ -10,6 +10,32 @@ namespace MyEvernote.BusinessLayer
 {
     public class CategoryManager : ManagerBase<Category>
     {
+        public override int Delete(Category category)
+        {
+            NoteManager noteManager = new NoteManager();
+            LikedManager likedManager = new LikedManager();
+            CommentManager commentManager = new CommentManager();
 
+            // Kategori ile ilişkili notların silinmesi gerekiyor.
+            foreach (Note note in category.Notes.ToList())
+            {
+
+                // Note ile ilişikili like'ların silinmesi.
+                foreach (Liked like in note.Likes.ToList())
+                {
+                    likedManager.Delete(like);
+                }
+
+                // Note ile ilişkili comment'lerin silinmesi
+                foreach (Comment comment in note.Commnets.ToList())
+                {
+                    commentManager.Delete(comment);
+                }
+
+                noteManager.Delete(note);
+            }
+
+            return base.Delete(category);
+        }
     }
 }
