545921ba0afded3b35d4927c1b1ede628e70895f
Murat Başeren <muratbaseren@hotmail.com>
2018-12-07 13:02:39 +0300

    Version info added.

--- MyEvernote.WebApp/Properties/AssemblyInfo.cs
+++ MyEvernote.WebApp/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MyEvernote.WebApp")]
+[assembly: AssemblyTitle("Udemy My Evernote Sample Project")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MyEvernote.WebApp")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("K. Murat Başeren")]
+[assembly: AssemblyProduct("Udemy My Evernote Sample Project")]
+[assembly: AssemblyCopyright("Copyright By K. Murat Başeren ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
